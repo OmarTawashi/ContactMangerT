b4f24dc4ac3bbd5c8170c928d54c5bab2b91e485
Omar Al tawashi <o.altawashi@unitone.ps>
2016-06-22 14:37:53 +0300

    Initial Commit

---
+++ ContactManger/ContactManger.h
@@ -0,0 +1,19 @@
+//
+//  ContactManger.h
+//  ContactManger
+//
+//  Created by Omar on 6/22/16.
+//  Copyright © 2016 Omar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ContactManger.
+FOUNDATION_EXPORT double ContactMangerVersionNumber;
+
+//! Project version string for ContactManger.
+FOUNDATION_EXPORT const unsigned char ContactMangerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ContactManger/PublicHeader.h>
+
+
