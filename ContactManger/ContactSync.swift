2b5de6738b0aabeda0702f67eb06738d15e3a7a8
Omar Al tawashi <o.altawashi@unitone.ps>
2016-06-25 10:54:17 +0300

    tt

---
+++ ContactManger/ContactSync.swift
@@ -0,0 +1,9 @@
+//
+//  ContactSync.swift
+//  ContactManger
+//
+//  Created by Omar on 6/25/16.
+//  Copyright © 2016 Omar. All rights reserved.
+//
+
+import Foundation
---
+++ ContactManger/ContactSyncDeleget.swift
@@ -0,0 +1,9 @@
+//
+//  ContactSyncDeleget.swift
+//  ContactManger
+//
+//  Created by Omar on 6/25/16.
+//  Copyright © 2016 Omar. All rights reserved.
+//
+
+import Foundation
